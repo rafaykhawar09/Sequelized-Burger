c47ef9340b8f21bf99c1cbd5b1fdb5138a0a2884
Rafay Khawar <rafaykhawar09@gmail.com>
2020-01-29 15:21:45 -0800

    pushing the finalized files

--- routes/burger-api-routes.js
+++ routes/burger-api-routes.js
@@ -1,8 +1,52 @@
 var db = require("../models");
 
-module.exports = function(app){
+module.exports = function (app) {
 
-     // app.post("/api/authors/", function(req, res){
-     //      db.Burger.create(req.params)
-     // })
+     // Create all our routes and set up logic within those routes where required.
+     app.get("/", function (req, res) {
+          db.Burger.findAll({ raw: true }).then(function (data) {
+               var hbsObject = {
+                    burgers: data
+               };
+               console.log(hbsObject);
+               res.render("index", hbsObject);
+          });
+     });
+
+     app.post("/api/burgers", function (req, res) {
+          db.Burger.create({
+               name: req.body.name,
+               devour: req.body.devour
+          }).then(function (result) {
+               // Send back the ID of the new quote
+               res.json({ id: result.insertId });
+          });
+     });
+
+     app.put("/api/burgers/:id", function (req, res) {
+
+          db.Burger.update(
+               {
+                    devour: req.body.devour
+               }, 
+               {
+                    where: {
+                         id: req.params.id
+                    }
+               }).then(function(data) {
+                    res.json(data);
+               });
+     });
+
+     app.delete("/api/burgers/:id", function (req, res) {
+
+          db.Burger.destroy({
+
+               where:{
+                    id: req.params.id
+               }
+          }).then(function(data) {
+               res.json(data);
+          });
+     });
 };
